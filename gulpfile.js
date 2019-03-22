b304bb9df49fa8b27fed5e9bbda806daa815fdf1
easonwa <chushibiao222@163.com>
2019-03-22 23:33:16 +0800

    nav及logo

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp'),
+	uglify = require('gulp-uglify'),	
+	cssnano = require('gulp-cssnano'),
+	sass =require('gulp-sass'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat');
+
+gulp.task('sass',()=>{
+	gulp.src('./src/sass/*.scss')
+	.pipe(sass())
+	.pipe(rename({"suffix" : ".min"}))
+	.pipe(gulp.dest('./dist/css'))
+	
+})
+gulp.task('js',()=>{
+	gulp.src('./src/js/*.js')
+	.pipe(uglify())
+	.pipe(concat('main.min.js'))
+	.pipe(gulp.dest('./dist/js'))
+})
+gulp.task('default',()=>{
+	gulp.watch(['./src/sass/*.scss'],['sass']);
+})
