eec7b6d36a3f81db64b07160639e79d6b24db31c
easonwa <chushibiao222@163.com>
2019-03-24 22:14:04 +0800

    banner图右和侧功能键 TV直播

--- src/js/index.js
+++ src/js/index.js
@@ -101,7 +101,58 @@ $(function(){
 			$(this).css('color','#555555')
 		})
 	})
-	
-	
-	
+	// 轮播按钮显隐
+	$('.bannerBox').mouseenter(function(){
+		$('.leftPoint').css('display','block');
+		$('.rightPoint').css('display','block')
+	})
+	$('.bannerBox').mouseleave(function(){
+		$('.leftPoint').css('display','none');
+		$('.rightPoint').css('display','none')
+	})
+	//右侧功能键效果
+	$('.right-funct .sign-in li:first-child').mouseenter(function(){
+		$('.right-funct .sign-in .first-li').css('display','block');
+	})
+	$('.right-funct .sign-in li:first-child').mouseleave(function(){
+		$('.right-funct .sign-in .first-li').css('display','none');
+	})
+	//关注
+	$('.right-funct .sign-in li:last-child').mouseenter(function(){
+		$('.right-funct .sign-in .last-li').css('display','block');
+	})
+	$('.right-funct .sign-in li:last-child').mouseleave(function(){
+		$('.right-funct .sign-in .last-li').css('display','none');
+	})
+	//下半部分功能键
+	$('.right-funct .right-bottom li:first-child').mouseenter(function(){
+		$('.right-funct .right-bottom li:first-child>img').css('display','block');
+	})
+	$('.right-funct .right-bottom li:first-child').mouseleave(function(){
+		$('.right-funct .right-bottom li:first-child>img').css('display','none');
+	})
+	$('.right-funct .right-bottom li:first-child').css('background-position','-47px -184px');
+	$('.right-funct .right-bottom li:first-child').hover(function(){
+		$(this).css('background-position','-47px -184px');
+	},function(){
+		$(this).css('background-position','0 -184px')
+	})
+	$('.right-funct .right-bottom li:eq(1)').mouseenter(function(){
+		$('.right-funct .right-bottom li:eq(1)>span').css('display','block');
+		
+	})
+	$('.right-funct .right-bottom li:eq(1)').mouseleave(function(){
+		$('.right-funct .right-bottom li:eq(1)>span').css('display','none');
+	})
+	$('.right-funct .right-bottom li:eq(2)').mouseenter(function(){
+		$('.right-funct .right-bottom li:eq(2)>span').css('display','block');
+		
+	})
+	$('.right-funct .right-bottom li:eq(2)').mouseleave(function(){
+		$('.right-funct .right-bottom li:eq(2)>span').css('display','none');
+	})
+	$('.right-funct .right-bottom li:eq(2)').click(function(){
+		$(window).scrollTop(0);
+		
+	})
 })
